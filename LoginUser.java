c003efbee19589b80484bc8a50880413af792227
P Nageswara Rao <nageshvkn@gamutgurus.com>
2017-06-25 16:12:47 +0530

    sfks;fks;f

--- LoginUser.java
+++ LoginUser.java
@@ -1,6 +1,8 @@
 public clas Login
 {
 	public static void main(String args[])
+	public static void main(String args[])
+	public static void main(String args[])
 		{
 			System.out.println("Hello World!");
 		}
